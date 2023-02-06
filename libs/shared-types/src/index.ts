f0bb64e6a2fdd210c506ad787ce9200d3e62c398
AlanFnz <alan.f@msn.com>
2023-02-06 18:21:57 -0300

    created shared type

--- libs/shared-types/src/index.ts
+++ libs/shared-types/src/index.ts
@@ -9,3 +9,17 @@ export interface Link {
   lastUpdate: null;
   type: string;
 }
+
+export interface Poster {
+  id: string;
+  name: string;
+  title: string;
+  printUrl: string;
+  instagramUrl: string;
+  img?: { data: Buffer; contentType: string };
+  visible: boolean;
+  print: boolean;
+  downloadable: boolean;
+  dateCreated: Date;
+  lastUpdate: Date;
+}
