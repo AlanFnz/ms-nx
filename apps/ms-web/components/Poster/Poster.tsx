178defa8a843abf96b49886c15da74717f07d7b9
AlanFnz <alan.f@msn.com>
2023-02-11 00:09:21 -0300

    updated poster component to use next js image

--- apps/ms-web/components/Poster/Poster.tsx
+++ apps/ms-web/components/Poster/Poster.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PosterImage } from './styledComponents';
 
-const Poster = ({ key, alt, src }) => (
-  <PosterImage key={key} alt={alt} src={src} />
-);
+const Poster = ({ key, alt, src }) => {
+  const [paddingTop, setPaddingTop] = useState('0');
+  return (
+    <div style={{ position: 'relative', paddingTop }}>
+      <PosterImage
+        key={key}
+        alt={alt}
+        src={src}
+        fill
+        onLoad={({ target }) => {
+          const { naturalWidth, naturalHeight } = target as HTMLImageElement;
+          setPaddingTop(`calc(100% / (${naturalWidth} / ${naturalHeight})`);
+        }}
+      />
+    </div>
+  );
+};
 
 export default Poster;
