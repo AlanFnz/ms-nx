f284994aa3a70ad362403c914e51852f0bf8190d
AlanFnz <alan.f@msn.com>
2023-02-13 18:15:22 -0300

    implementing async thunk

--- apps/ms-web/state/slices/posters.js
+++ apps/ms-web/state/slices/posters.js
@@ -1,49 +1,69 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
   data: [],
   currentPoster: {},
-  errorMsg: '',
+  errorMessage: '',
+  loading: false,
 };
 
+const fetchPosters = createAsyncThunk('posters/fetchPosters', () => {
+  axios
+    .get('https://api.printful.com/store/products', {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-type': 'Application/json',
+        Authorization: `Bearer ${process.env.PRINTFUL_TOKEN}`,
+      },
+    })
+    .then((response) => response.data);
+});
+
 const postersSlice = createSlice({
-  name: 'Posters',
+  name: 'posters',
   initialState,
   reducers: {
     reducers: {
-      postersRequestError(state, action) {
-        state.errorMsg = action.payload;
-      },
-      postersRequestDataSuccess(state, action) {
-        state.data = action.payload.data;
-      },
       setCurrentPoster(state, action) {
         state.currentPoster = action.payload.poster;
       },
-      cleanPosters: () => initialState,
+      cleanPostersReducer: () => initialState,
       cleanPostersData() {
         state.data = initialState.data;
       },
       cleanCurrentPoster(state) {
         state.currentPoster = initialState.currentPoster;
       },
-      cleanErrorMsg() {
-        state.errorMsg = '';
+      cleanErrorMessage() {
+        state.errorMessage = '';
       },
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchPosters.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchPosters.fulfilled, (state, action) => {
+      state.loading = false;
+      state.data = action.payload;
+      state.errorMessage = '';
+    });
+    builder.addCase(fetchPosters.rejected, (state, action) => {
+      state.loading = false;
+      state.errorMessage = action.error.message;
+    });
   },
 });
 
 const { actions, reducer } = postersSlice;
 
 export const {
-  postersRequestError,
-  postersRequestDataSuccess,
   setCurrentPoster,
-  cleanPosters,
+  cleanPostersReducer,
   cleanPostersData,
   cleanCurrentPoster,
-  cleanErrorMsg,
+  cleanErrorMessage,
 } = actions;
 
 export default reducer;
