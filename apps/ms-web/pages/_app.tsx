6aaa092c022eeeb47087347e36be2ad99313fa4d
AlanFnz <alan.f@msn.com>
2023-02-14 20:02:07 -0300

    providing store

--- apps/ms-web/pages/_app.tsx
+++ apps/ms-web/pages/_app.tsx
@@ -1,12 +1,16 @@
 import { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import store from '../state/store';
+
 import Head from 'next/head';
 import LogoHeader from '../components/LogoHeader/LogoHeader';
 import SocialLinks from '../components/SocialLinks/SocialLinks';
+
 import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Provider store={store}>
       <Head>
         <title>Mystery Skools</title>
       </Head>
@@ -15,7 +19,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <SocialLinks />
         <Component {...pageProps} />
       </main>
-    </>
+    </Provider>
   );
 }
 
